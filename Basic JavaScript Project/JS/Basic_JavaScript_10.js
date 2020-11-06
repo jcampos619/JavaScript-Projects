125904089ec462fe43864905293dc20964e7b7da
jcampos619 <64349514+jcampos619@users.noreply.github.com>
2020-11-05 22:48:45 -0800

    Add files via upload

--- Basic JavaScript Project/JS/Basic_JavaScript_10.js
+++ Basic JavaScript Project/JS/Basic_JavaScript_10.js
@@ -1,3 +1,4 @@
+/* This function counts to 10 */
 function Call_Loop() {
     var Digit= "";
     var X= 1;
@@ -8,16 +9,18 @@ function Call_Loop() {
     document.getElementById("Loop").innerHTML= Digit;
 }
 
-var Instruments= ("Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute");
+/* This lists ALL of the instruments */
+var Instruments= ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
 var Content= "";
-function for_Loop(params) {
+var Y;
+function for_Loop() {
     for (Y = 0; Y < Instruments.length; Y++) {
         Content += Instruments[Y] + "<br>";
     }
-    document.getElementById("List_of_instruments").innerHTML= Content;
+    document.getElementById("List_of_Instruments").innerHTML= Content;
 }
 
-function array_Function(params) {
+function array_Function() {
     var Cat_Picture= []
     Cat_Picture[0]= "sleeping";
     Cat_Picture[1]= "playing";
@@ -26,7 +29,7 @@ function array_Function(params) {
     document.getElementById("Array").innerHTML= "In this picture, the car is " + Cat_Picture[3] + ".";
 }
 
-function constant_function(params) {
+function constant_function() {
     const Musical_Instrument= {type:"guitar", brand:"Fender", color:"black"};
     Musical_Instrument.color= "blue";
     Musical_Instrument.price= "$900";
@@ -34,6 +37,7 @@ function constant_function(params) {
 
 }
 
+/* This whole thing below is to show the different function of 'let' and 'var' */
 var X= 82;
 document.write(X);
 {
@@ -42,12 +46,12 @@ document.write(X);
 }
 document.write("<br>" + X);
 
-let car= {
-    make: "Mazda",
-    model: "Miata",
-    year: "1992",
-    color: "red",
-    description: function (params) {
+let car = {
+    make: "Mazda ",
+    model: "Miata ",
+    year: "1992 ",
+    color: "red ",
+    description: function () {
         return "The car is a " + this.year + this.make + this.model;
     }
 };
